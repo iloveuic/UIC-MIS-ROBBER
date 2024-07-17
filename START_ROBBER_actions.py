7461e3f34cb9e89f3310cd00b0f6165d5bb1cefc
Henry King <87317232+henrykingcn@users.noreply.github.com>
2024-07-17 22:16:13 +0800

    Update START_ROBBER_actions.py

--- START_ROBBER_actions.py
+++ START_ROBBER_actions.py
@@ -25,6 +25,8 @@ if not os.path.exists('UIC_MIS_ROBBER/courseList.csv'):
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(options=options,service=service)
 
     # 打开指定的URL并登录
@@ -168,6 +170,9 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-port=9222')
+options.add_argument('--disable-gpu')
+
 
 # 初始化Chrome浏览器
 driver = webdriver.Chrome(options=options,service=service)
